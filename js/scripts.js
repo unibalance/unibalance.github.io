da53995c19df1e91bf36ff9a745dbc3792426762
webteam <webteam@convergentglobe.com>
2018-01-18 10:53:18 +0530

    Integrate firebase with contact form

--- js/scripts.js
+++ js/scripts.js
@@ -36,4 +36,56 @@ $(function () {
         }
         return false;
     }
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBZ-RqEvuDbXZt0j8SegGEkctiT273Tqfk",
+        authDomain: "unibalance-india-enquiry.firebaseapp.com",
+        databaseURL: "https://unibalance-india-enquiry.firebaseio.com",
+        projectId: "unibalance-india-enquiry",
+        storageBucket: "unibalance-india-enquiry.appspot.com",
+        messagingSenderId: "1073173740332"
+    };
+    firebase.initializeApp(config);
+
+    // Reference Enquiries Collection
+    var enquiriesRef = firebase.database().ref('enquiries');
+
+    //Function Save enquiry details to firebase
+    function saveMessage(name, email, phone, message) {
+        var newEnquiryRef =  enquiriesRef.push();
+        newEnquiryRef.set({
+            name: name,
+            email: email,
+            phone: phone,
+            message: message
+        });
+    }
+
+    // Function to save the enquiry form data
+    function submitEnquiry(e) {
+        
+        // Prevent the default action of the form
+        e.preventDefault();
+
+        // Get the form input values
+        var name = $('#user-name').val();
+        var email = $('#user-email').val();
+        var phone = $('#user-tel').val();
+        var message = $('#user-message').val();
+
+        // Save enquiry details to firebase after collecting form input values
+        saveMessage(name, email, phone, message);
+
+        // Show Success/Failure Alert
+        $(".form-status-alert").text("Your message has been sent");
+
+        setTimeout(function() {
+            $(".form-status-alert").text("");            
+        }, 3000);
+
+    }
+
+    $(".enquiry-form form").on("submit", submitEnquiry);
+
 });
